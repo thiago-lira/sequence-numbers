dbea90e4a6556b53eac35727d8a6318382dba006
Thiago Lira <lira_thiago@msn.com>
2024-02-16 01:02:09 -0300

    Layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainContent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainContent from './components/MainContent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainContent
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #333;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
 }
 </style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="column-align">
+    <div class="output-display">
+      <h1>Oi, Mundo!</h1>
+    </div>
+
+    <div class="virtual-keyboard">
+      <div class="keys">
+        <button
+          v-for="label in keyLabels"
+          :key="label"
+          class="key"
+        >
+          {{ label }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  setup() {
+    const keyLabels = Array.from(Array(10).keys())
+
+    return {
+      keyLabels
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.column-align {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  > * {
+    flex-grow: 1;
+  }
+}
+
+.output-display {
+  padding-top: 100px;
+}
+
+.virtual-keyboard {
+  button {
+    background: #300;
+    border: 1px solid #900;
+    border-radius: 6px;
+    color: #f00;
+    font-size: 26px;
+    margin: 4px;
+    padding: 10px 20px;
+  }
+}
+</style>
