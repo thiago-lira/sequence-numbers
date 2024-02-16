db6fc772f13516531fb95c73768013c285fb3df6
Thiago Lira <lira_thiago@msn.com>
2024-02-16 05:03:08 -0300

    Create stats page

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import MainContent from './pages/MainContent.vue'
+import StatsContent from './pages/StatsContent.vue'
 
 import './registerServiceWorker'
 
 const routes = [
-  { path: '/', component: MainContent }
+  { path: '/', component: MainContent },
+  { path: '/stats', component: StatsContent }
 ]
 const router = createRouter({
   history: createWebHashHistory(),
---
+++ src/pages/StatsContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    Stats
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'StatsContent',
+}
+</script>
